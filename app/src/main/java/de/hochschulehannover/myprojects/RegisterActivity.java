fc0be8e7b8e3c13b41d0b63b78c7211f1b61bfa0
Ole Bause <ole@bausecreative.de>
2021-12-05 19:27:00 +0100

    Registrierungsbildschirm überarbeitet

--- app/src/main/java/de/hochschulehannover/myprojects/RegisterActivity.java
+++ app/src/main/java/de/hochschulehannover/myprojects/RegisterActivity.java
@@ -1,11 +1,14 @@
 package de.hochschulehannover.myprojects;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -19,6 +22,8 @@ public class RegisterActivity extends AppCompatActivity {
 
     EditText registerMailEditText;
     EditText registerPasswordEditText;
+    Button registerButton;
+    Toolbar toolbar;
 
     private FirebaseAuth mAuth;
 
@@ -34,10 +39,23 @@ public class RegisterActivity extends AppCompatActivity {
 
         registerMailEditText = findViewById(R.id.mailEditText);
         registerPasswordEditText = findViewById(R.id.registerPasswordEditText);
+        registerButton = findViewById(R.id.registerButton);
+        toolbar = findViewById(R.id.toolbar);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mail = registerMailEditText.getText().toString();
+                String password = registerPasswordEditText.getText().toString();
+                register(mail, password);
+            }
+        });
+
+        setupActionBar();
     }
 
-    public void register(View view) {
-        mAuth.createUserWithEmailAndPassword(registerMailEditText.getText().toString(), registerPasswordEditText.getText().toString())
+    public void register(String mail, String password) {
+        mAuth.createUserWithEmailAndPassword(mail, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
@@ -56,5 +74,22 @@ public class RegisterActivity extends AppCompatActivity {
                         }
                     }
                 });
+    }
+
+    // Custom ActionBar initialisieren
+    private void setupActionBar() {
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_back_24dp);
+        }
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
--- app/src/main/java/de/hochschulehannover/myprojects/WelcomeActivity.java
+++ app/src/main/java/de/hochschulehannover/myprojects/WelcomeActivity.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    Button registerButton;
+    Button loginButton;
 
     private FirebaseAuth mAuth;
 
@@ -30,21 +34,30 @@ public class WelcomeActivity extends AppCompatActivity {
 
         // Firebase initialisieren
         mAuth = FirebaseAuth.getInstance();
-    }
 
-    public void register(View view) {
-        Intent intent = new Intent(this, RegisterActivity.class);
-        startActivity(intent);
-    }
+        registerButton = findViewById(R.id.welcomeRegisterButton);
+        loginButton = findViewById(R.id.welcomeLoginButton);
 
-    public void login(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WelcomeActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void goToProjects() {
         Intent intent = new Intent(this, ProjectListActivity.class);
-        intent.putExtra("name", "Benutzername"); //TODO: Benutzernamen aus Firebase holen
+        intent.putExtra("name", "Benutzername"); //TODO: Name aus Firebase holen
         startActivity(intent);
     }
 
