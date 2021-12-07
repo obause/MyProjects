78c4b4830ecdd027bcd300d6d6f9cb5df929be64
Ole Bause <ole@bausecreative.de>
2021-12-07 18:40:20 +0100

    Bildauswahl und Medienzugriff für Profilseite

--- app/src/main/java/de/hochschulehannover/myprojects/ProfileActivity.java
+++ app/src/main/java/de/hochschulehannover/myprojects/ProfileActivity.java
@@ -1,14 +1,26 @@
 package de.hochschulehannover.myprojects;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.common.util.ArrayUtils;
+
+import java.io.IOException;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import de.hochschulehannover.myprojects.firebase.FirestoreClass;
@@ -20,6 +32,12 @@ public class ProfileActivity extends BaseActivity {
     CircleImageView userImage;
     EditText nameEditText;
     EditText emailEditText;
+
+    // TODO: In Constants Klasse packen
+    private static int READ_STORAGE_PERMISSION_CODE = 1;
+    private static int PICK_IMAGE_REQUEST_CODE = 2;
+
+    private Uri selectedImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +53,26 @@ public class ProfileActivity extends BaseActivity {
         setupActionBar();
 
         new FirestoreClass().loginUser(this);
+
+        userImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(ProfileActivity.this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    showImageChooser();
+                } else {
+                    ActivityCompat.requestPermissions(ProfileActivity.this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            READ_STORAGE_PERMISSION_CODE);
+                }
+            }
+        });
+    }
+
+    private void showImageChooser() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // TODO: Veraltete Funktion ersetzen
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE);
     }
 
     public void setUserDetails(User user) {
@@ -47,6 +85,39 @@ public class ProfileActivity extends BaseActivity {
 
         nameEditText.setText(user.name);
         emailEditText.setText(user.email);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE_REQUEST_CODE && data.getData() != null) {
+            selectedImageUri = data.getData();
+
+            try {
+                Glide
+                        .with(this)
+                        .load(Uri.parse(selectedImageUri.toString()))
+                        .centerCrop()
+                        .placeholder(R.drawable.ic_user_place_holder)
+                        .into(userImage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == READ_STORAGE_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                showImageChooser();
+            } else {
+                showErrorSnackBar("Berechtigungen wurden verweigert");
+            }
+        }
     }
 
     // Custom ActionBar initialisieren
