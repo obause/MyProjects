652373d4b769deed2c1e8e2a36f8f2605c810c6d
Constantin <cbirkobein@gamil.com>
2021-12-28 15:23:16 +0100

    einfache FAQ Seite

--- app/src/main/java/de/hochschulehannover/myprojects/FaqActivity.java
+++ app/src/main/java/de/hochschulehannover/myprojects/FaqActivity.java
@@ -7,11 +7,36 @@ import android.widget.TextView;
 
 public class FaqActivity extends AppCompatActivity {
     TextView headlineFaqTextView;
+    TextView frage1;
+    TextView frage2;
+    TextView frage3;
+    TextView frage4;
+    TextView frage5;
+    TextView frage6;
+
+    TextView antwort1;
+    TextView antwort2;
+    TextView antwort3;
+    TextView antwort4;
+    TextView antwort5;
+    TextView antwort6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
         headlineFaqTextView = findViewById(R.id.headlineFaqTextView);
+        frage1 = findViewById(R.id.frage1);
+        frage2 = findViewById(R.id.frage2);
+        frage3 = findViewById(R.id.frage3);
+        frage4 = findViewById(R.id.frage4);
+        frage5 = findViewById(R.id.frage5);
+        frage6 = findViewById(R.id.frage6);
+        antwort1 = findViewById(R.id.antwort1);
+        antwort2 = findViewById(R.id.antwort2);
+        antwort3 = findViewById(R.id.antwort3);
+        antwort4 = findViewById(R.id.antwort4);
+        antwort5 = findViewById(R.id.antwort5);
+        antwort6 = findViewById(R.id.antwort6);
     }
 }
